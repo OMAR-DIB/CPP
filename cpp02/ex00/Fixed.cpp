64d4fc785638e494ea64bb5f00960d4fa2bf24d2
OMAR-DIB <omardib759@gmail.com>
2025-02-15 18:48:18 +0200

    finish ex00

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,1 +1,39 @@
 #include "Fixed.hpp"
+
+Fixed::Fixed(/* args */) : store(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed& other){
+    std::cout << "Copy assignment operator called" << std::endl;
+    // Self-assignment check 
+    if (this != &other){
+        store = other.getRawBits();
+    }
+
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return store;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	store = raw;
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -13,25 +13,11 @@ public:
     Fixed(/* args */);
     ~Fixed();
     Fixed(const Fixed& cp);
-    Fixed& operator=(const Fixed& other){
-        // Self-assignment check 
-        if (this == &other){
-            return *this;
-        }
-        store = other.store;
-
-        return *this;
-    }
+    Fixed& operator=(const Fixed& other);
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
 };
 
-Fixed::Fixed(/* args */) : store(0)
-{
-    std::cout << "Default constructor called" << std::endl;
-}
-
-Fixed::~Fixed()
-{
-}
 
 
 #endif
